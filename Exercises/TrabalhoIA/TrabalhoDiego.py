524e1534ff7493e23f8085199e40e7e94b675c68
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2024-09-25 19:30:55 -0300

    Update TrabalhoDiego.py

--- Exercises/TrabalhoIA/TrabalhoDiego.py
+++ Exercises/TrabalhoIA/TrabalhoDiego.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("Vigorou.csv")
+colunas_independentes = ["Categoria-do-veiculo","Numero-de-passageiros","Capacidade-de-porta-malas","Possui-ar-condicionado","Tipo-de-cambio"]
+
 print('=' * 40)
 print('BEM VINDO AO ALUGUEL DE CARROS!')
 print('=' * 40)
@@ -27,53 +33,12 @@ while cam not in '12':
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 '''
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
 
 df = pd.read_csv("data.csv")
 colunas_independentes = ["numero-de-alunos","temperatura-ambiende","N-Computadores","capacidade-de-sala","Horario-do-dia"]
-colunas_dependentes = ["Temperadura"]
-
-
-dados_x = df[colunas_independentes]
-dados_y = df[colunas_dependentes]
-
-
-modelo = LinearRegression().fit(dados_x, dados_y)
-
-num_alunos = 21
-temperatura = 27
-N_computadores = 27
-capacidade_de_sala = 30
-Horario_do_dia = 20
-
-valores = np.array([num_alunos,temperatura,N_computadores,capacidade_de_sala,Horario_do_dia])
-
-
-print("A temperatura prevista   de:", modelo.predict([valores]))'''
+colunas_dependentes = ["Temperadura"]'''
